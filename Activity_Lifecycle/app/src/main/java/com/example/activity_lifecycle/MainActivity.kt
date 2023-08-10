9df78def573579c85d97892ae4eb952265d26798
MahdiHaeri <mahdihaerim@gmail.com>
2023-08-10 19:03:11 +0330

    Add Activity_Lifecycle pratice

---
+++ Activity_Lifecycle/app/src/main/java/com/example/activity_lifecycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.activity_lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.activity_lifecycle.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        println("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume");
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("ohDestroy")
+    }
+}
