68ed69d1a8ef1fc03fe10704228615ec01fa783f
MahdiHaeri <mahdihaerim@gmail.com>
2023-08-20 09:57:10 +0330

    Add BottomNavigation

---
+++ BottomNavigation/app/src/main/java/com/mahdihaeri/bottomnavigation/MainActivity.kt
@@ -0,0 +1,127 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.mahdihaeri.bottomnavigation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.mahdihaeri.bottomnavigation.ui.theme.BottomNavigationTheme
+
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean = false,
+    val badgeCount: Int? = null
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomNavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val items = listOf<BottomNavigationItem>(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            hasNews = false,
+        ),
+        BottomNavigationItem(
+            title = "Chat",
+            selectedIcon = Icons.Filled.Email,
+            unselectedIcon = Icons.Outlined.Email,
+            hasNews = false,
+            badgeCount = 45
+        ),
+        BottomNavigationItem(
+            title = "Setting",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings,
+            hasNews = true,
+        ),
+    )
+
+    var selectedItemIndex by remember { mutableStateOf(0) }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        alwaysShowLabel = false,
+                        selected = selectedItemIndex == index, onClick = { selectedItemIndex = index},
+                        label = { Text(text = item.title) },
+                        icon = {
+                            BadgedBox(
+                                badge = {
+                                    if (item.badgeCount != null) {
+                                        Badge {
+                                            Text(text = item.badgeCount.toString())
+                                        }
+                                    } else if (item.hasNews) {
+                                        Badge()
+                                    }
+                                }
+                            ) {
+                                Icon(imageVector = if (index == selectedItemIndex) { item.selectedIcon} else {item.unselectedIcon}, contentDescription = item.title)
+                            }
+                        }
+
+                    )
+                }
+            }
+        }
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ApplicationPreview() {
+    BottomNavigationTheme() {
+        MyApp()
+    }
+}
